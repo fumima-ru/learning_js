7e0b4c7d9fbfb611066f081d8a1ea3b4b5a486ff
fumima-ru <marumefumiko@gmail.com>
2023-11-08 11:54:53 +0900

    topic: jsコードレシピ集P29
    計算プログラムを書く～四則演算～
    上記を学びました
    #1

--- js/learning_js.js
+++ js/learning_js.js
@@ -1,6 +1,8 @@
-alert('jsファイルにjsを記述する');
+console.log(100 + 200); /* 加算 */
+console.log(200 - 50);  /* 減算 */
+console.log(100 * 3);   /* 乗算 */
+console.log(100 / 2);   /* 除算 */
+console.log(10 % 4);    /* 除算の余り */
+console.log(2 ** 3);    /* べき乗 */
 
-const a = 10;
-const b = 20;
-const sum = a + b;
-console.log(sum);
+
