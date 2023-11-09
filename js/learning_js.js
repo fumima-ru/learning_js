ff30189af3727c28f7930f628d1c1295862d3a17
fumima-ru <marumefumiko@gmail.com>
2023-11-09 16:40:12 +0900

    topic: jsコードレシピ集P33-P35
    定数を使う～const～
    上記を学びました
    #1

--- js/learning_js.js
+++ js/learning_js.js
@@ -1,39 +1,39 @@
-// 変数の値を省略
-let noValue1;
-console.log(noValue1);
+// 定数の値は省略できない
+/* const noValue1;
+console.log(noValue1); */
 // 値を代入
-noValue1 = 1;
-console.log(noValue1);
+/* noValue1 = 1;
+console.log(noValue1); */
 
 // 数値を代入
-let number1 = 7;
-let number2 = 10;
+const number1 = 7;
+const number2 = 10;
 console.log(number1 + number2);
 // 値の再代入
-number2 = 20; 
-console.log(number1 + number2);
+/* number2 = 20; 
+console.log(number1 + number2); */
 
 // 文字列
-let nickname = 'fumima-ru';
+const nickname = 'fumima-ru';
 console.log(`ニックネーム：${nickname}`);
 // 文字列の値を結合
-let introduction = 'ニックネームは';
+const introduction = 'ニックネームは';
 console.log(introduction + nickname);
 
 
 // 日付
-let currentDate  = new Date();
+const currentDate  = new Date();
 console.log(currentDate);
 
 // 関数
-let greetFunction = () => console.log('こんにちは');
+const greetFunction = () => console.log('こんにちは');
 greetFunction();
 
-// 変数を変数に代入
-let value1 = 100;
-let value2 = value1 + value1;
+// 定数を定数に代入
+const value1 = 100;
+const value2 = value1 + value1;
 console.log(value1, value2);
 
-// 変数をまとめて宣言する
-let a = 1 , b = 2 , c = 3;
+// 定数をまとめて宣言する
+const a = 1 , b = 2 , c = 3;
 console.log(a, b, c);
