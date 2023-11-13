b0379f24448362b6846d245e17584cb51c6b7722
fumima-ru <marumefumiko@gmail.com>
2023-11-13 17:24:04 +0900

    topic: jsコードレシピ集P41-P43
    関数～function～
    上記を学びました
    #1

--- js/learning_js.js
+++ js/learning_js.js
@@ -1,35 +1,63 @@
-// 複合代入演算子
-let a = 10;
-let b = 20;
-a += b;
-console.log(a);
+// 関数
+function f1(number) {
+  const RESULT = number + 1;
+  return RESULT; 
+}
+console.log(`f1:${f1(1)}`);
 
-let c = 'くいだおれ';
-let d = '太郎';
-c += d;
-console.log(c);
+function f2(number1,number2,number3) {
+  const RESULT = number1 + number2 + number3;
+  return RESULT;
+}
+console.log(`f2:${f2(1, 2, 3)}`);
 
-let e = 100;
-let f = 200;
-e *= f;
-console.log(e);
+// 引数のない関数
+function f3() {
+  console.log('f3:引数のない関数');
+  return 3;
+}
+f3();
+console.log(`f3:${f3()}`);
 
-const A = 7;
-let g = 10;
-g += A;
-console.log(g);
+// 戻り値(return)のない関数
+function f4() {
+  console.log('f4:戻り値(return)のない関数');
+}
+f4();
 
-let h = 10;
-let i = 3;
-h **= i;
-console.log(h);
+// return後の処理は実行されない
+function f5() {
+  return 5;
+  console.log('return後の処理は実行されない');
+}
+console.log(`f5:${f5()}`);
 
-let j = 10;
-let k = 2;
-j /= 2;
-console.log(j);
+// returnは複数 記述できる
+function f6(number1, number2) {
+  if (number1 > 100) {
+    return number1;
+  }
+  return number2;
+}
+console.log(`f6:${f6(60, 160)}`);
 
-let l = 10;
-let m = 3;
-l %= 3;
-console.log(l);
+// returnは複数 記述できるが値を返すとその後の処理は実行されない
+function f7(number1, number2, number3) {
+  if (number1 > 100) {
+    return number1;
+  } else if (number2 > 150) {
+    return number2;
+    console.log('値を返すとその後の処理は実行されない');
+  } else {
+    return number3;
+  }
+}
+console.log(`f7:${f7(70, 170, 270)}`);
+
+// 戻り値を定数に代入する
+function f8(number1, number2) {
+  const RESULT = number1 * number2;
+  return RESULT;
+}
+const F8_RESULT = f8(8, 10);
+console.log(`f8:${F8_RESULT}`);
