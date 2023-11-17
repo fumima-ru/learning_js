58a86f9417da36be8f0716d49a59742163d3b8ae
fumima-ru <marumefumiko@gmail.com>
2023-11-17 14:56:44 +0900

    topic: jsコードレシピ集P47-P48
    残余引数～for...of文～
    上記を学びました
    残余引数やfor...of文の簡単な例文も記述しました
    #1

--- js/learning_js.js
+++ js/learning_js.js
@@ -1,5 +1,32 @@
-const F1 = (parameter) => {
-  console.log(`${parameter}を仮引数 (parameter)に渡して出力する。`);
+/** 
+ * 引数の合計値を返す関数
+ * @param prices 関数の仮引数（パラメーター）
+ * @returns {number} 関数の戻り値 {型}
+ */
+function calcSum(...prices) {
+  let result = 0;
+  for (const VALUE of prices) {
+    result += VALUE;
+  }
+  return result;
 }
 
-F1('実引数 (argument)');
+const RESULT_1 = calcSum(10, 20);
+console.log(RESULT_1);
+const RESULT_2 = calcSum(100, 200, 300);
+console.log(RESULT_2);
+
+
+// for...of文
+for (const VALUE of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
+  console.log(VALUE);
+}
+
+
+// 残余引数
+const REST_ARGUMENTS = (...array) => {
+  const RESULT = array;
+  console.log(RESULT);
+}
+
+REST_ARGUMENTS(11, 12, 13, 14, 15, 16, 17);
