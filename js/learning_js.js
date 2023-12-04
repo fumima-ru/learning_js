1f280992aeb4db8850044ed6861587f45d3ff4c1
fumima-ru <marumefumiko@gmail.com>
2023-12-04 15:19:43 +0900

    topic: jsコードレシピ集 番外編【組み込みのメソッド～Object.keys(obj),Object.values(obj),Object.entries(obj)～】
    Object.keys(obj) – キーの配列を返す
    Object.values(obj) – 値の配列を返す
    Object.entries(obj) – ペアの配列を返す[key, value]
    上記を学びました
    #1

--- js/learning_js.js
+++ js/learning_js.js
@@ -1,48 +1,10 @@
-// while (条件式) {
-  // 繰り返す処理;
-  // ・
-  // ・
-// }
+const fruit = {name:'柿', color:'橙色', season: '秋'};
 
-// while文
-console.log('【numberToAdd1】');
-let numberToAdd1 = 0;
-while (numberToAdd1 < 10) {
-  console.log(numberToAdd1);
-  numberToAdd1++;
-}
+// Object.keys(obj)
+console.log(Object.keys(fruit));
 
-// while文 break文
-console.log('【numberToAdd2】');
-let numberToAdd2 = 0;
-while (numberToAdd2 < 10) {
-  console.log(numberToAdd2);
-  numberToAdd2++;
-  // 3になったら その旨を出力してbreak;
-  if (numberToAdd2 === 3) {
-    console.log('0から2までを出力して終了しました');
-    break;
-  }
-}
+// Object.values(obj)
+console.log(Object.values(fruit));
 
-// while文 continue文
-console.log('【numberToAdd3】');
-let numberToAdd3 = 0;
-while (numberToAdd3 < 10) {
-  // 偶数の場合1を足してスキップ
-  if (numberToAdd3 % 2 === 0) {
-    numberToAdd3++;
-    continue;
-  }
-  console.log(numberToAdd3);
-  numberToAdd3++;
-}
-
-// while文 配列
-console.log('【array[i]】');
-let array = [1, 3, 5, 7, 9];
-let i = 0;
-while (i < array.length) {
-  console.log(array[i]);
-  i++;
-}
+// Object.entries(obj)
+console.log(Object.entries(fruit));
