cb2605bb2948c06bf1918cb012e1f55d85832ae8
fumima-ru <marumefumiko@gmail.com>
2023-12-04 16:27:22 +0900

    topic: jsコードレシピ集 番外編【オブジェクト情報の取得メソッドとネスト】
    Object.keys(obj)、Object.values(obj)、Object.entries(obj)等の
    オブジェクト情報の取得メソッドとネストについて
    上記を学びました
    #1

--- js/learning_js.js
+++ js/learning_js.js
@@ -1,10 +1,21 @@
-const fruit = {name:'柿', color:'橙色', season: '秋'};
+const FRUIT = {
+  name:'柿',
+  color:'橙色',
+  season: '秋',
+  variety: {
+    variety1: '富有柿',
+    variety2: '次郎柿',
+    variety3: '筆柿',
+    variety4:'紀の川柿',
+    variety5: '西条柿'
+  }
+};
 
 // Object.keys(obj)
-console.log(Object.keys(fruit));
+console.log(Object.keys(FRUIT));
 
 // Object.values(obj)
-console.log(Object.values(fruit));
+console.log(Object.values(FRUIT));
 
 // Object.entries(obj)
-console.log(Object.entries(fruit));
+console.log(Object.entries(FRUIT));
