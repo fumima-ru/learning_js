c918dbcd1f6440c69a910fd684a0e590e79bcf83
fumima-ru <marumefumiko@gmail.com>
2023-12-22 14:34:32 +0900

    topic:制作物からコードを学ぶ【～toFixed( )メソッド～】
    toFixed( )メソッド
    数値を文字列に変換する際に使用される
    小数点以下の桁数を引数に指定できる(指定した桁数未満の場合は0で埋められる)
    返り値は四捨五入された文字列
    
    上記を学びました
    #1

--- js/learning_js.js
+++ js/learning_js.js
@@ -4,11 +4,8 @@ function calculateBMI() {
   const WEIGHT = parseFloat(document.getElementById('weight').value);
 
   // BMIを計算
-  const bmi = WEIGHT / ((HEIGHT / 100) * (HEIGHT / 100));
-
+  const BMI = WEIGHT / (HEIGHT / 100) ** 2;
+  
   // 結果を表示
-  document.getElementById('result_display').innerText = bmi.toFixed(2);
-
-  console.log(HEIGHT, typeof(HEIGHT));
-  console.log(WEIGHT);
-}
+  document.getElementById('result_display').innerText = BMI.toFixed(2);
+};
