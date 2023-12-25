7adfb00265c0b02ae12051b9a1848a5c5f3adf0c
fumima-ru <marumefumiko@gmail.com>
2023-12-25 14:24:21 +0900

    topic:制作物からコードを学ぶ【～getElementBy_,querySelector_～】
    要素を取得するための代表的なメソッド例
    ～メソッドを使用して要素を取得～
    ・id名を使用して要素を取得(一意)
    ・クラス名を使用して要素を取得(複数)
    ・タグ名を使用して要素を取得(複数,NodeListを返す)
    ・name属性を使用して要素を取得(複数)
    ・CSSセレクタを使用して要素を取得(一致する最初の要素のみ)
    ・CSSセレクタを使用して要素を取得(複数)
    
    上記を学びました
    #1

--- js/learning_js.js
+++ js/learning_js.js
@@ -9,3 +9,18 @@ function calculateBMI() {
   // 結果を表示
   document.getElementById('result_display').innerText = BMI.toFixed(2);
 };
+
+// 要素を取得するための代表的なメソッド例
+
+// id名を使用して要素を取得(一意)
+document.getElementById('【例】header');
+// クラス名を使用して要素を取得(複数)
+document.getElementsByClassName('【例】wrapper');
+// タグ名を使用して要素を取得(複数,NodeListを返す)
+document.getElementsByTagName('【例】span');
+// name属性を使用して要素を取得(複数)
+document.getElementsByName('【例】name');
+// CSSセレクタを使用して要素を取得(一致する最初の要素のみ)
+document.querySelector('【例】#header');
+// CSSセレクタを使用して要素を取得(複数)
+document.querySelectorAll('【例】.flex');
