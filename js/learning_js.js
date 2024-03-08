494868e6fb5eb3de29b5307d38ef3fa5bf0deacf
fumima-ru <marumefumiko@gmail.com>
2024-03-08 17:10:45 +0900

    topic:エラーが怖くなくなる本から学ぶ 【js～sort( )メソッド～】
    sort( )メソッド
    配列の要素をソートする
    
    配列やオブジェクトに使用できる
    戻り値は配列
    デフォルトでは文字列として比較されるが比較関数を指定することもできる
    元の配列が変更されるため注意が必要
    
    #1

--- js/learning_js.js
+++ js/learning_js.js
@@ -1,8 +1,40 @@
 // プリントデバッグ
 
-console.log('クリア前のメッセージ');
+// sort( )メソッド
+const MEMBERS_LAST_NAME = [
+  'サトウ', 'スズキ', 'タカハシ', 'タナカ', 'ワタナベ', 'イトウ', 'ヤマモト'
+];
+console.log(MEMBERS_LAST_NAME.sort());
 
-// コンソールをクリア
-console.clear();
 
-console.log('クリア後のメッセージ');
+// ※下記のコードを細分化して学んでいますが下記のコードに間違いがあれば都度、修正します
+
+// 2桁のランダムな数値の配列を生成
+/* const RANDOM_ARRAY = [12, 5, 34, 22, 8, 41, 18, 91, 30, 55, 67, 49].sort((a, b) => a - b);
+console.log(RANDOM_ARRAY); */
+
+// 二分探索関数
+/* function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) {
+            return `ターゲット ${target} は配列の ${mid} 番目にあります。`;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return `ターゲット ${target} は配列に見つかりませんでした。`;
+} */
+
+// 使用例
+/* const TARGET = 22; // 探す値
+console.log("ランダムな配列:", RANDOM_ARRAY);
+console.log("探す値:", TARGET);
+console.log(binarySearch(RANDOM_ARRAY, TARGET)); */
